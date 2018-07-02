800d839c847a649e852a31259c43a1fac9aaa25a
mhaagens <martin.haagensli@gmail.com>
2018-07-03 00:10:22 +0200

    jokes

--- src/models/post.js
+++ src/models/post.js
@@ -1,9 +1,24 @@
 const { gql } = require("apollo-server");
 
 const mockPosts = [
-  { id: 1, title: "Hello World", ownerId: 1 },
-  { id: 2, title: "Hello Bird", ownerId: 2 },
-  { id: 3, title: "Hello Turd", ownerId: 1 }
+  {
+    id: 1,
+    title:
+      "Helvetica and Times New Roman walk into a bar. Get out of here! shouts the bartender. We don't serve your type!",
+    ownerId: 1
+  },
+  {
+    id: 2,
+    title:
+      "Why do we tell actors to break a leg? Because every play has a cast.",
+    ownerId: 2
+  },
+  {
+    id: 3,
+    title:
+      "Did you hear about the mathematician who’s afraid of negative numbers? He'll stop at nothing to avoid them.",
+    ownerId: 1
+  }
 ];
 
 module.exports.postDefs = gql`
